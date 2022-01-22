85df30c22ae24eb4f50563793cd6e72c43d553b9
박현우(Park Hyun Woo) <gusdn3477@naver.com>
2022-01-22 14:46:17 +0900

    스프링 빈 조회 - 상속 관계

--- src/test/java/com/example/core/beanfind/ApplicationContextSameBeanFindTest.java
+++ src/test/java/com/example/core/beanfind/ApplicationContextSameBeanFindTest.java
@@ -1,7 +1,9 @@
 package com.example.core.beanfind;
 
 import com.example.core.AppConfig;
+import com.example.core.RateDiscountPolicy;
 import com.example.core.discount.DiscountPolicy;
+import com.example.core.discount.FixDiscountPolicy;
 import com.example.core.member.MemberRepository;
 import com.example.core.member.MemoryMemberRepository;
 import org.junit.jupiter.api.Assertions;
@@ -19,44 +21,59 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ApplicationContextSameBeanFindTest {
 
-    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(SameBeanConfig.class);
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
 
     @Test
-    @DisplayName("타입으로 조회 시 같은 타입이 둘 이상 있으면, 중복 오류가 발생한다")
-    void findBeanByTypeDuplicate(){
+    @DisplayName("부모 타입으로 조회 시, 자식이 둘 이상 있으면 중복 오류가 발생한다")
+    void findBeanByParentTypeDuplicate(){
+        DiscountPolicy bean = ac.getBean(DiscountPolicy.class);
         assertThrows(NoUniqueBeanDefinitionException.class,
-                () -> ac.getBean(MemberRepository.class));
+                () -> ac.getBean(DiscountPolicy.class));
     }
 
     @Test
-    @DisplayName("타입으로 조회시 같은 타입이 둘 이상 있으면, 빈 이름을 지정하면 된다")
-    void findBeanByName(){
-        MemberRepository memberRepository = ac.getBean("memberRepository1", MemberRepository.class);
-        assertThat(memberRepository).isInstanceOf(MemberRepository.class);
-    }
-
-    @Test
-    @DisplayName("특정 타입을 모두 조회하기")
-    void findAllBeanByType(){
-        Map<String, MemberRepository> beansOfType = ac.getBeansOfType(MemberRepository.class);
-        for (String key : beansOfType.keySet()) {
-            System.out.println("key = " + key + " value" + beansOfType.get(key));
-        }
-        System.out.println("beanofType = " + beansOfType);
+    @DisplayName("부모 타입으로 모두 조회하기")
+    void findAllBeanByParentType(){
+        Map<String, DiscountPolicy> beansOfType = ac.getBeansOfType(DiscountPolicy.class);
         assertThat(beansOfType.size()).isEqualTo(2);
+        for (String key : beansOfType.keySet()) {
+            System.out.println("key = " + key + " value = " + beansOfType.get(key));
+        }
+    }
+
+    @Test
+    @DisplayName("부모 타입으로 모두 조회하기 - Object")
+    void findAllBeanByObjectType(){
+        Map<String, Object> beansOfType = ac.getBeansOfType(Object.class);
+        for (String key : beansOfType.keySet()) {
+            System.out.println("key = " + key + " value = " + beansOfType.get(key));
+        }
+    }
+
+    @Test
+    @DisplayName("부모 타입으로 조회 시, 자식이 둘 이상 있으면 빈 이름을 지정하면 된다")
+    void findBeanByParentTypeBeanName(){
+        DiscountPolicy rateDiscountPolicy = ac.getBean("rateDiscountPolicy", DiscountPolicy.class);
+        assertThat(rateDiscountPolicy).isInstanceOf(RateDiscountPolicy.class);
+    }
+
+    @Test
+    @DisplayName("특정 하위 타입으로 조회")
+    void findBeanBySubType(){
+        RateDiscountPolicy bean = ac.getBean(RateDiscountPolicy.class);
+        assertThat(bean).isInstanceOf(RateDiscountPolicy.class);
     }
 
     @Configuration
-    static class SameBeanConfig {
-
+    static class TestConfig{
         @Bean
-        public MemberRepository memberRepository1() {
-            return new MemoryMemberRepository();
+        public DiscountPolicy rateDiscountPolicy(){
+            return new RateDiscountPolicy();
         }
 
         @Bean
-        public MemberRepository memberRepository2() {
-            return new MemoryMemberRepository();
+        public DiscountPolicy fixDiscountPolicy(){
+            return new FixDiscountPolicy();
         }
     }
 }
