e45e75fdfa5f14116cfef02876c0f4c5acd0d10a
박현우(Park Hyun Woo) <gusdn3477@naver.com>
2022-01-29 22:06:06 +0900

    롬복과 최신 트렌드

--- src/main/java/com/example/core/order/OrderServiceImpl.java
+++ src/main/java/com/example/core/order/OrderServiceImpl.java
@@ -5,23 +5,20 @@ import com.example.core.discount.FixDiscountPolicy;
 import com.example.core.member.Member;
 import com.example.core.member.MemberRepository;
 import com.example.core.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService{
 
+    // 윈도우의 경우 ctrl + f12 => 현재 메소드 다 보여줌
     // 수정자 주입을 포함한 나머지 주입 방식은 모두 생성자 이후에 호출되므로,
     // 필드에 'final' 키워드를 사욯알 수 없다. 오직 생성자 주입 방식만 'final' 키워드를 사용할 수 있다.
     // 항상 생성자 주입하기! 가끔 옵션이 필요한 겨우 수정자 주입 사옹. 필드는 X => 테스트 시에 값을 넣을 수 없음..
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
-
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
