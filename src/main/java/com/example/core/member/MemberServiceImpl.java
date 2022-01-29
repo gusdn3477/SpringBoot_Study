50bc03323d9c63ded29759f47f0e2dc9c8c283e8
박현우(Park Hyun Woo) <gusdn3477@naver.com>
2022-01-29 16:45:19 +0900

    컴포넌트 스캔과 의존관계 자동 주입 시작하기

--- src/main/java/com/example/core/member/MemberServiceImpl.java
+++ src/main/java/com/example/core/member/MemberServiceImpl.java
@@ -1,9 +1,14 @@
 package com.example.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MemberServiceImpl implements MemberService{
 
     private final MemberRepository memberRepository;
 
+    @Autowired
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
@@ -12,12 +17,10 @@ public class MemberServiceImpl implements MemberService{
     public void join(Member member) {
         memberRepository.save(member);
     }
-
     @Override
     public Member findMember(Long memberId) {
         return memberRepository.findById(memberId);
     }
-
     // 테스트 용도
     public MemberRepository getMemberRepository(){
         return memberRepository;
