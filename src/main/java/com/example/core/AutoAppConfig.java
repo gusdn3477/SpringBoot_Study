6da29e76c112b476a46ef71f3c0b4c1564f9d315
박현우(Park Hyun Woo) <gusdn3477@naver.com>
2022-01-29 17:17:08 +0900

    탐색 위치와 기본 스캔 대상

--- src/main/java/com/example/core/AutoAppConfig.java
+++ src/main/java/com/example/core/AutoAppConfig.java
@@ -5,6 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
 @Configuration
-@ComponentScan(excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class))
+@ComponentScan(
+        basePackages = "com.example.core.member",
+        basePackageClasses = AutoAppConfig.class,
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class))
+// basePackages 지정하지 않을 시(default) : @ComponentScan을 붙인 클래스부터 시작하여 하위 패키지를 뒤진다.
+// 패키지 위치를 지정하지 안고, 설정 정보 클래스의 위치를 프로젝트 최상단에 두는 것을 권장한다.
 public class AutoAppConfig {
 }
