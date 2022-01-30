3f14bad98ad57695200dcc0e9ddf06af45c91d5f
박현우(Park Hyun Woo) <gusdn3477@naver.com>
2022-01-31 01:06:37 +0900

    빈 생명주기 콜백 시작

---
+++ src/test/java/com/example/core/lifecycle/BeanLifeCycle.java
@@ -0,0 +1,29 @@
+package com.example.core.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifeCycle {
+
+//    스프링 빈은 객체 생성 후 의존관계 주입을 하는 라이프사이클을 거친다.
+    @Test
+    public void lifeCycleTest(){
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+        NetworkClient client = ac.getBean(NetworkClient.class);
+        ac.close();
+    }
+
+    @Configuration
+    static class LifeCycleConfig{
+        @Bean
+        public NetworkClient networkClient(){
+            NetworkClient networkClient = new NetworkClient();
+            networkClient.setUrl("http://hello-spring.dev");
+            return networkClient;
+        }
+    }
+}
