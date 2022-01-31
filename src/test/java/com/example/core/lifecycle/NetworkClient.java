dbc3da75157f58939050be067ce32d1bbb41509c
박현우(Park Hyun Woo) <gusdn3477@naver.com>
2022-01-31 21:54:01 +0900

    빈 등록 초기화, 소멸 메서드

--- src/test/java/com/example/core/lifecycle/NetworkClient.java
+++ src/test/java/com/example/core/lifecycle/NetworkClient.java
@@ -3,7 +3,7 @@ package com.example.core.lifecycle;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient  {
 
     private String url;
 
@@ -29,15 +29,13 @@ public class NetworkClient implements InitializingBean, DisposableBean {
         System.out.println("close " + url);
     }
 
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        System.out.println("NetworkClient.afterPropertiesSet");
+    public void init(){
+        System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메세지");
     }
 
-    @Override
-    public void destroy() throws Exception {
-        System.out.println("NetworkClient.destory");
+    public void close(){
+        System.out.println("NetworkClient.close");
     }
 }
